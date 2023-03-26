d36c9a3b635d4323c6c4e7674f350e02a2e26302
AbhishekChauhan28 <chauhanabhi280@gmail.com>
2023-03-26 23:56:15 +0530

    predict pipeline

--- src/components/model_training.py
+++ src/components/model_training.py
@@ -41,17 +41,69 @@ class ModelTrainer:
                 test_array[:,-1]
             )
             
+            '''
+            Models Training :
+            '''
+            
             models = {
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                "k-Neighbors Regressor": KNeighborsRegressor(),
+                "AdaBoost Regressor": AdaBoostRegressor(),
                 "XGBRegressor": XGBRegressor(),
                 "CatBoosting Regression": CatBoostRegressor(),
+                "K-Neighbor Regression": KNeighborsRegressor(),
             }
             
-            model_report:dict = evaluate_models(x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test, models = models)
+            '''
+            Hyper - Parameter Tunning :
+            '''
+            
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regression":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "K-Neighbor Regression":{
+                    'n_neighbors': [5,7,9,11],
+                    # 'weights': ['uniform', 'distance'],
+                    # 'algorithm': ['ball_tree', 'kd_tree', 'brute'] 
+                }
+                
+            }
+            
+            model_report:dict = evaluate_models(x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test, models = models, param = params)
             
             # get best model score from dict
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,9 @@ import dill
 import numpy as np
 import pandas as pd
 
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 
@@ -22,16 +25,27 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(x_train, y_train, x_test, y_test, models):
+def evaluate_models(x_train, y_train, x_test, y_test, models, param):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+            
+            #gs = GridSearchCV(model, para, cv = 5, n_jobs = n_jobs, verbose = verbose, refit = refit)
+            gs = GridSearchCV(model, para, cv = 5)
+            gs.fit(x_train, y_train)
+            
+            #rs = RandomizedSearchCV(model, para, cv =7)
+            #rs.fit(x_train, y_train)
+            
+            model.set_params(**gs.best_params_)
+            model.fit(x_train, y_train)
             
             '''
             Training the model
             '''
-            model.fit(x_train, y_train) 
+            # model.fit(x_train, y_train) 
             
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
@@ -43,6 +57,14 @@ def evaluate_models(x_train, y_train, x_test, y_test, models):
             report[list(models.keys())[i]] = test_model_score
         
         return report
+    
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
     
     except Exception as e:
         raise CustomException(e, sys)
